f891e51ce70e90787926a0f7600e5e50e8196207
Miłosz Pawłowski <milosz.pawlowski.torun@wp.pl>
2023-04-11 19:17:47 +0200

    Finished header
    Finished header update - only need to change main photo

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&family=Nunito+Sans:wght@300;400;600;700&family=Open+Sans:ital,wght@0,300;0,400;0,700;0,800;1,400&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
+    
 </head>
 <body>
     <?php require_once("./modules/header.php"); ?>
--- modules/header.php
+++ modules/header.php
@@ -2,4 +2,14 @@
     <div class="menu-container">
         <img src="./photos/logo.png" alt="Logo High Speedway Challenge League">
     </div>
+    <div class="header-banner">
+        <h1>HIGH SPEEDWAY<br>CHALLENGE LEAGUE</h1>
+        <p>Zapraszamy do gry w naszej lidze już dziś!<br>Spróbuj swoich sił w następnej kolejce!</p>
+        <button>ZOBACZ WIĘCEJ</button>
+    </div>
+    <div class="social-links">
+        <a href="https://www.facebook.com/profile.php?id=100088668614084" target="_blank"><img src="./photos/facebook.png" alt="Ikona Facebook'a"></a>
+        <a href="https://www.youtube.com/@HSCL" target="_blank"><img src="./photos/youtube.png" alt="Ikona Youtube"></a>
+        <a href="https://www.tiktok.com/@highscleague" target="_blank"><img src="./photos/tiktok.png" alt="Ikona TikToka"></a>
+    </div>
 </header>
